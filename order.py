d49b399ac6a7f1bac7134033d73dd79827c932d7
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 22:25:38 -0500

    Update order.py

--- order.py
+++ order.py
@@ -7,3 +7,8 @@ class Order:
         self.inventory = inventory
         self.items_by_category = {"Burgers": [], "Sides": [], "Drinks": []}
         self.combos = []
+
+    async def add_item(self, item_id):
+        stock_level, item = await asyncio.gather(
+            self.inventory.get_stock(item_id), self.inventory.get_item(item_id)
+        )
