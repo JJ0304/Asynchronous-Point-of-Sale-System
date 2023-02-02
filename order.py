6907502cdb1b748280b25c94e6e0cb8646ffbb84
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 22:24:59 -0500

    Update order.py

--- order.py
+++ order.py
@@ -1,2 +1,9 @@
 import asyncio
 from combo import Combo
+
+
+class Order:
+    def __init__(self, inventory):
+        self.inventory = inventory
+        self.items_by_category = {"Burgers": [], "Sides": [], "Drinks": []}
+        self.combos = []
