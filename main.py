2092f08920c7fcfeb19f5057a84b337c4f91cca9
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 23:12:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import asyncio
 from inventory import Inventory
 from order import Order
 
+def display_catalogue(catalogue):
+    burgers = catalogue["Burgers"]
+    sides = catalogue["Sides"]
+    drinks = catalogue["Drinks"]
