3c71f342ec9b1f839d849fad6b42125678614157
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 23:15:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,41 @@ def display_catalogue(catalogue):
     burgers = catalogue["Burgers"]
     sides = catalogue["Sides"]
     drinks = catalogue["Drinks"]
+
+    print("--------- Burgers -----------\n")
+    for burger in burgers:
+        item_id = burger["id"]
+        name = burger["name"]
+        price = burger["price"]
+        print(f"{item_id}. {name} ${price}")
+
+    print("\n---------- Sides ------------")
+    for side in sides:
+        sizes = sides[side]
+
+        print(f"\n{side}")
+        for size in sizes:
+            item_id = size["id"]
+            size_name = size["size"]
+            price = size["price"]
+            print(f"{item_id}. {size_name} ${price}")
+
+    print("\n---------- Drinks ------------")
+    for beverage in drinks:
+        sizes = drinks[beverage]
+
+        print(f"\n{beverage}")
+        for size in sizes:
+            item_id = size["id"]
+            size_name = size["size"]
+            price = size["price"]
+            print(f"{item_id}. {size_name} ${price}")
+
+    print("\n------------------------------\n")
+
+
+async def get_order(inventory, num_items):
+    order = Order(inventory)
+    tasks = []
+
+
