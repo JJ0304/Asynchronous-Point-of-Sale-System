4adad6b6fbe0179fb7f87811b3b84ca477b94d05
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 21:25:59 -0500

    Update combo.py

--- combo.py
+++ combo.py
@@ -1,2 +1,8 @@
 class Combo:
     DISCOUNT = 0.15
+
+    def __init__(self, burger, side, drink):
+        self.burger = burger
+        self.side = side
+        self.drink = drink
+        self.price = self._calculate_price()
