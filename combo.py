5bfb651c8e21aa1e7a73ab77e94ef0360775737c
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 21:26:31 -0500

    Update combo.py

--- combo.py
+++ combo.py
@@ -6,3 +6,7 @@ class Combo:
         self.side = side
         self.drink = drink
         self.price = self._calculate_price()
+
+    def _calculate_price(self):
+        subtotal = self.burger["price"] + self.side["price"] + self.drink["price"] 
+        return round(subtotal * (1 - Combo.DISCOUNT), 2)
