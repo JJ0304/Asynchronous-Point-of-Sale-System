a3f1162dcdd6033db6238af94630572af79fb4f1
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 22:38:35 -0500

    Update inventor.py

--- inventor.py
+++ inventor.py
@@ -1,2 +1,15 @@
 import asyncio
 import random
+
+
+class Inventory:
+    def __init__(self):
+        self.catalogue = {
+            "Burgers": [
+                {"id": 1, "name": "Python Burger", "price": 5.99},
+                {"id": 2, "name": "C Burger", "price": 4.99},
+                {"id": 3, "name": "Ruby Burger", "price": 6.49},
+                {"id": 4, "name": "Go Burger", "price": 5.99},
+                {"id": 5, "name": "C++ Burger", "price": 7.99},
+                {"id": 6, "name": "Java Burger", "price": 7.99}
+            ],
