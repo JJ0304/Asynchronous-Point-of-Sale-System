b8bc5069f6eed5cb16ce8d2a7d338b56a45bf666
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-01 22:49:25 -0500

    Update inventor.py

--- inventor.py
+++ inventor.py
@@ -45,3 +45,8 @@ class Inventory:
         self._generate_item_lookup_dict()
         self.stock = {i + 1: random.randint(0,15) for i in range(len(self.items))}
         self.stock_lock = asyncio.Lock()
+
+    def _generate_item_lookup_dict(self):
+        self.items = {}
+        for category in self.catalogue:
+            category_collection = self.catalogue[category]
